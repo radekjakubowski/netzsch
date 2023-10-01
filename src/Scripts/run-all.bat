ac725075d1d97b49d0ceeb3057f43da5c244c090
Radek Jakubowski <jakubowskiumieszcz@gmail.com>
2023-10-01 10:57:36 +0200

    got rid of batch

--- src/Scripts/run-all.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Running webserver...
-start powershell.exe -ExecutionPolicy Bypass -File ".\1RunServer.ps1"
-echo Running local clients...
-powershell.exe -ExecutionPolicy Bypass -File ".\2RunLocalClients.ps1" -NoNewWindow
-
-echo Startup completed.
